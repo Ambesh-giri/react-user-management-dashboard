8714b28eacd626e74927db74bd8919fe6b18d17c
Ambesh-giri <ambeshgiri73@gmial.com>
2025-11-30 03:13:04 +0530

    updated UI

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,7 +1,7 @@
 export default function UserCard({user}){
     return (
         <div className="usercard">
-            <h3>Name : {user.firstName} </h3>
+            <h3>Name : {user.firstName} {user.lastName}</h3>
             <p><b>Email : </b>{user.email}</p>
             <p><b>Gender : </b>{user.gender}</p>
             <p><b>City : </b>{user.address.city}</p>
